4dea6d4e1f5764855c89ac063cfeb67f1bfc3137
Anil Ganipineni <akganipineni@taxilla.com>
2020-06-14 21:02:25 +0530

    0.2 final versions

--- src/main/resources/cassandra.sql
+++ src/main/resources/cassandra.sql
@@ -1,14 +1,26 @@
-create table scheduled_tasks (
-    task_name varchar(100),
-    task_instance varchar(100),
-    task_data blob,
-    execution_time TIMESTAMP(6),
-    picked NUMBER(1,0),
-    picked_by varchar(50),
-    last_success TIMESTAMP(6),
-    last_failure TIMESTAMP(6),
-    consecutive_failures NUMBER(19,0),
-  last_heartbeat TIMESTAMP(6),
-    version NUMBER(19,0),
-    PRIMARY KEY (task_name, task_instance)
-);
+CREATE TABLE scheduled_tasks (
+    task_name text,
+    task_instance text,
+    version int,
+    consecutive_failures int,
+    execution_time timestamp,
+    last_failure timestamp,
+    last_heartbeat timestamp,
+    last_success timestamp,
+    picked boolean,
+    picked_by text,
+    task_data text,
+    PRIMARY KEY (task_name, task_instance, version)
+) WITH read_repair_chance = 0.0
+   AND dclocal_read_repair_chance = 0.1
+   AND gc_grace_seconds = 864000
+   AND bloom_filter_fp_chance = 0.01
+   AND caching = { 'keys' : 'ALL', 'rows_per_partition' : 'NONE' }
+   AND comment = ''
+   AND compaction = { 'class' : 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold' : 32, 'min_threshold' : 4 }
+   AND compression = { 'chunk_length_in_kb' : 64, 'class' : 'org.apache.cassandra.io.compress.LZ4Compressor' }
+   AND default_time_to_live = 0
+   AND speculative_retry = '99PERCENTILE'
+   AND min_index_interval = 128
+   AND max_index_interval = 2048
+   AND crc_check_chance = 1.0;
